bfdb7eeaf9f33488cc41a7307eba2109388eb33b
Wesley-waka <wesleywaka77@gmail.com>
2024-03-10 06:42:37 +0300

    cloudinary file upload

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -54,9 +54,18 @@ export async function PUT(request: NextRequest,{params}: {params: {id: string}})
     }
 
 
-export function DELETE(request: NextRequest,{params}: {params: {id: number}}){
-    if(params.id>10)
+export async function DELETE(request: NextRequest,{params}: {params: {id: string}}){
+    const user = await prisma.user.findUnique({
+        where: {id: parseInt(params.id)}
+    })
+    
+    if(!user)
         return NextResponse.json({error: 'User not found'},{status:404})
+    
+        prisma.user.delete({
+        where: {id: user.id}
+    });
+
     return NextResponse.json({});
 
 }
