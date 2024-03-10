905787a7d0ecb92ff71dee11f24ee444f60a19ce
Wesley-waka <wesleywaka77@gmail.com>
2024-03-10 21:35:17 +0300

    Authentication using Auth.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+import { getServerSession } from 'next-auth'
 import Image from 'next/image'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getServerSession(authOptions)
+  
   return (
-      <div>Dashboard</div>
+      <div>
+        <h1>Hello {session && <span>{session.user!.name}</span>}</h1>
+        <div>Dashboard</div>
+      </div>
   )
 }
---
+++ prisma/migrations/20240310181407_add_password/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `emailVerified` DATETIME(3) NULL,
+    `hashedPassword` VARCHAR(191) NOT NULL,
+    `image` VARCHAR(191) NULL,
+    `followers` INTEGER NOT NULL DEFAULT 0,
+    `isActive` BOOLEAN NOT NULL DEFAULT true,
+    `registeredAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
