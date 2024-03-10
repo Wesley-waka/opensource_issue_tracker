4d195072d607da7bf92c84c586bf7166096fffa5
Wesley-waka <wesleywaka77@gmail.com>
2024-03-10 18:41:07 +0300

    Setup Auth.js sessions

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,9 +1,11 @@
-'use client'
+'use client';
+
 import Link from 'next/link'
 import React from 'react'
 import { AiFillBug } from 'react-icons/ai';
 import {usePathname} from 'next/navigation';
 import classnames from 'classnames';
+import { useSession } from 'next-auth/react';
 
 interface links{
     href: string;
@@ -12,6 +14,10 @@ interface links{
 
 const Navbar = () => {
     const currentPath = usePathname();
+    const {status,data: session} = useSession();
+    if(status === 'loading') return null;
+
+
     const Links: links[] =[
         {href:"/dashboard",label: "Dashboard"},
         {href:"/issues",label: "Issues"}
@@ -19,6 +25,10 @@ const Navbar = () => {
     return (
     <nav className='flex flex-row space-x-4 border-b mb-5 px-5 h-14 items-center'>
         <Link href="/"><AiFillBug/></Link>
+        <Link href='/api/auth/signin'>Login</Link>
+        <Link href='/users'>Users</Link>
+        {status === 'authenticated' && <div>{session.user!.name}</div>}
+        {status === 'unauthenticated' && <Link href="/api/auth/signin"></Link>}
         <ul className='flex space-x-6'>
             {Links.map((link)=>
             <li
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import Navbar from './Navbar'
 import { Suspense } from 'react'
+import AuthProvider from './auth/Provider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,13 +17,16 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
+  
   return (
     <html lang="en">
       <body className={inter.className}>
+      <AuthProvider>
         <Navbar/>
         <main>
             {children}
-        </main>        
+        </main>  
+      </AuthProvider>
       </body>
     </html>
   )
